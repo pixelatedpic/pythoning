30a1a3e44cd4f1b22a737a8bf0576600291975b0
Mohamed Afzal <mohamedafzal@gmail.com>
2018-07-27 21:41:43 +0500

    small changes

--- conDD.py
+++ conDD.py
@@ -1,18 +1,14 @@
 def dm(x):
     degrees = int(x) // 100
     minutes = x - 100*degrees
-    #print(degrees)
-    #print(minutes)
-    conval =  degrees + minutes/60
-    return conval
-#r = dm(07332.48362)
-#print(r)
-#def decimal_degrees(degrees, minutes):
-#    print(degrees)
-#    print(minutes)
-#    t =  degrees + minutes/60
-#    print(t) 
+    print(degrees)
+    print(minutes)
+    return degrees, minutes
+
+def decimal_degrees(degrees, minutes):
+    	# print(degrees)
+    	# print(minutes)
+	t =  degrees + minutes/60
+	print(t) 
 #	return degrees + minutes/60
 #print (decimal_degrees(*dm(0412.74943)))
-#dm(07332.48362)
-#decimal_degrees(73,32.483619)
